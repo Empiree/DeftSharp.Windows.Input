5284761639463bcc7a3b9845648bfa0abf4fb03d
NullPTR <156743576+nullptrException100@users.noreply.github.com>
2024-03-10 12:28:40 +0100

    Update ManipulatorTests.cs

--- DeftSharp.Windows.Input.Tests/Keyboard/ManipulatorTests.cs
+++ DeftSharp.Windows.Input.Tests/Keyboard/ManipulatorTests.cs
@@ -22,13 +22,13 @@ public class ManipulatorTests : IDisposable
         {
             keyboardManipulator1.Prevent(Key.A);
             keyboardManipulator2.Prevent(Key.A);
-            
+
             Assert.Equal(keyboardManipulator1.LockedKeys.Count(), keyboardManipulator2.LockedKeys.Count());
-            
+
             keyboardManipulator1.Release(Key.A);
         });
     }
-    
+
     [Fact]
     public void Manipulator_Test2()
     {
@@ -39,11 +39,15 @@ public class ManipulatorTests : IDisposable
         {
             keyboardManipulator1.Prevent(Key.A);
             keyboardManipulator2.Prevent(Key.A);
-            
-            keyboardManipulator1.Release(Key.A);
+
+            Assert.Equal(keyboardManipulator1.LockedKeys.Count(), keyboardManipulator2.LockedKeys.Count());
+
+            keyboardManipulator1.ReleaseAll();
+
+            Assert.Empty(keyboardManipulator2.LockedKeys);
         });
     }
-    
+
     [Fact]
     public void Manipulator_Test3()
     {
@@ -57,12 +61,15 @@ public class ManipulatorTests : IDisposable
             keyboardManipulator2.Prevent(Key.W);
             keyboardManipulator3.Prevent(Key.R);
 
-            keyboardManipulator1.Release(Key.Q);
-            keyboardManipulator2.Release(Key.W);
-            keyboardManipulator3.Release(Key.R);
+            Assert.Equal(keyboardManipulator1.LockedKeys.Count(), keyboardManipulator2.LockedKeys.Count());
+            Assert.Equal(keyboardManipulator2.LockedKeys.Count(), keyboardManipulator3.LockedKeys.Count());
+
+            keyboardManipulator1.ReleaseAll();
+
+            Assert.Empty(keyboardManipulator2.LockedKeys);
         });
     }
-    
+
     [Fact]
     public void Manipulator_Test4()
     {
@@ -75,11 +82,13 @@ public class ManipulatorTests : IDisposable
             keyboardManipulator1.Prevent(Key.Q);
             keyboardManipulator2.Prevent(Key.W);
 
-            keyboardManipulator3.Release(Key.Q);
-            keyboardManipulator3.Release(Key.W);
+            keyboardManipulator3.ReleaseAll();
+
+            Assert.Empty(keyboardManipulator1.LockedKeys);
+            Assert.Empty(keyboardManipulator2.LockedKeys);
         });
     }
-    
+
     [Fact]
     public void Manipulator_Test5()
     {
@@ -94,14 +103,16 @@ public class ManipulatorTests : IDisposable
                         keyboardManipulator1.Prevent(Key.Q);
                         keyboardManipulator2.Prevent(Key.W);
                     }
-                    
+
+                    Assert.NotEmpty(keyboardManipulator1.LockedKeys);
+
                     keyboardManipulator1.Release(Key.Q);
                     keyboardManipulator1.Release(Key.W);
                 }
             }
         });
     }
-    
+
     [Fact]
     public void Manipulator_Test6()
     {
@@ -111,27 +122,21 @@ public class ManipulatorTests : IDisposable
         _emulator.Run(() =>
         {
             var keyboardManipulator2 = new KeyboardManipulator();
-            keyboardManipulator2.KeyPrevented += key =>
-            {
-                counterOfPreventEvents++;
-            };
-            
+            keyboardManipulator2.KeyPrevented += key => { counterOfPreventEvents++; };
+
             keyboardManipulator1.Prevent(Key.A);
             keyboardManipulator1.Prevent(Key.A);
-            
+
             var keyboardManipulator3 = new KeyboardManipulator();
             keyboardManipulator3.Prevent(Key.E);
 
-            keyboardManipulator3.KeyReleased += key =>
-            {
-                counterOfReleasedEvents++;
-            };
-            
+            keyboardManipulator3.KeyReleased += key => { counterOfReleasedEvents++; };
+
             Assert.Equal(2, counterOfPreventEvents);
-            
+
             keyboardManipulator3.Release(Key.A);
             keyboardManipulator3.Release(Key.E);
-            
+
             Assert.Equal(counterOfReleasedEvents, counterOfPreventEvents);
         });
     }
