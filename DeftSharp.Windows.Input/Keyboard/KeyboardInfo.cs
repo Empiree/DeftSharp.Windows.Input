045e9edf0d008c98274904f6fd5640d92742059e
Empiree <Empiree@users.noreply.github.com>
2024-04-04 00:04:22 +0300

    Refactoring

--- DeftSharp.Windows.Input/Keyboard/KeyboardInfo.cs
+++ DeftSharp.Windows.Input/Keyboard/KeyboardInfo.cs
@@ -7,33 +7,13 @@ namespace DeftSharp.Windows.Input.Keyboard;
 /// </summary>
 public sealed class KeyboardInfo : IKeyboardInfo
 {
-    private KeyboardLayout? _layout;
-    private KeyboardType? _type;
-
     /// <summary>
     /// Gets the current keyboard layout.
     /// </summary>
-    public KeyboardLayout Layout
-    {
-        get
-        {
-            _layout ??= GetLayout();
-
-            return _layout;
-        }
-    }
+    public KeyboardLayout Layout => GetLayout();
 
     /// <summary>
     /// Gets the current keyboard type.
     /// </summary>
-    public KeyboardType Type
-    {
-        get
-        {
-            _type ??= GetKeyboardType();
-
-            return _type;
-        }
-    }
-
+    public KeyboardType Type => GetKeyboardType();
 }
