8c5542a544774f7dc5d1e47cb373d7fe83d36a22
kirill <kirillaromad@mail.ru>
2024-04-28 14:09:07 +0300

    Add NumberListener subscribe and unsubscribe tests

---
+++ DeftSharp.Windows.Input.Tests/Keyboard/NumberListenerSubscribeTests.cs
@@ -0,0 +1,27 @@
+﻿namespace DeftSharp.Windows.Input.Tests.Keyboard;
+
+public sealed class NumberListenerSubscribeTests
+{
+    private readonly NumButton[] _numKeys =
+    {
+        new(Key.D1, 1), new(Key.D2, 2), new(Key.D3, 3), new(Key.D4, 4), new(Key.D5, 5),
+        new(Key.D6, 6), new(Key.D7, 7), new(Key.D8, 8), new(Key.D9, 9), new(Key.D0, 0)
+    };
+
+    [Fact]
+    public async void NumberListener_Subscribe()
+    {
+        var keyboardListener = new KeyboardListener();
+        var numberListener = new NumberListener(keyboardListener);
+
+        await Task.Run(() =>
+        {
+            numberListener.Subscribe(_ => { });
+
+            Assert.True(keyboardListener.IsListening, "Keyboard listener is not listening subscription events.");
+            Assert.True(numberListener.IsListening, "Number listener is not listening subscription events.");
+            Assert.Equal(keyboardListener.Keys.Count(), _numKeys.Length);
+            Assert.True(_numKeys.All(n => keyboardListener.Keys.Select(s => s.Key).Contains(n.Key)));
+        });
+    }
+}
---
+++ DeftSharp.Windows.Input.Tests/Keyboard/NumberListenerUnsubscribeTests.cs
@@ -0,0 +1,47 @@
+﻿namespace DeftSharp.Windows.Input.Tests.Keyboard;
+
+public sealed class NumberListenerUnsubscribeTests
+{
+    private readonly NumButton[] _numKeys =
+    {
+        new(Key.D1, 1), new(Key.D2, 2), new(Key.D3, 3), new(Key.D4, 4), new(Key.D5, 5),
+        new(Key.D6, 6), new(Key.D7, 7), new(Key.D8, 8), new(Key.D9, 9), new(Key.D0, 0)
+    };
+
+    [Fact]
+    public async void NumberListener_SubscribeUnsubscribe()
+    {
+        var keyboardListener = new KeyboardListener();
+        var numberListener = new NumberListener(keyboardListener);
+
+        await Task.Run(() =>
+        {
+            numberListener.Subscribe(_ => { });
+            numberListener.Unsubscribe();
+
+            Assert.False(keyboardListener.IsListening);
+            Assert.False(numberListener.IsListening);
+        });
+    }
+
+    [Fact]
+    public async void NumberListener_SubscribeUnsubscribeOnlyNumberListenerSubscriptions()
+    {
+        var keyboardListener = new KeyboardListener();
+        var numberListener = new NumberListener(keyboardListener);
+        var keys = _numKeys.Select(n => n.Key).ToArray();
+
+        await Task.Run(() =>
+        {
+            keyboardListener.Subscribe(keys, _ => { });
+
+            numberListener.Subscribe(_ => { });
+            numberListener.Unsubscribe();
+
+            Assert.True(keyboardListener.IsListening);
+            Assert.Equal(keyboardListener.Keys.Count(), keys.Length);
+            Assert.True(keys.All(k => keyboardListener.Keys.Select(s => s.Key).Contains(k)));
+            Assert.False(numberListener.IsListening);
+        });
+    }
+}
