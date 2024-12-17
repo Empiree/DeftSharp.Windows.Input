5eff52ef5e5a2e72fc31a100c559216124ba91ce
Gleb Tsoy <admin@zeptosky.com>
2024-12-17 21:27:14 +1100

    Add keyboard layout enum and relevant updates

--- DeftSharp.Windows.Input.Tests/Keyboard/LetterListenerUnsuscribeTest.cs
+++ DeftSharp.Windows.Input.Tests/Keyboard/LetterListenerUnsuscribeTest.cs
@@ -2,36 +2,6 @@
 
 public sealed class LetterListenerUnsubscribeTests
 {
-    private readonly LetterButton[] _letKeys =
-    {
-        new(Key.A, "A"),
-        new(Key.B, "B"),
-        new(Key.C, "C"),
-        new(Key.D, "D"),
-        new(Key.E, "E"),
-        new(Key.F, "F"),
-        new(Key.G, "G"),
-        new(Key.H, "H"),
-        new(Key.I, "I"),
-        new(Key.J, "J"),
-        new(Key.K, "K"),
-        new(Key.L, "L"),
-        new(Key.M, "M"),
-        new(Key.N, "N"),
-        new(Key.O, "O"),
-        new(Key.P, "P"),
-        new(Key.Q, "Q"),
-        new(Key.R, "R"),
-        new(Key.S, "S"),
-        new(Key.T, "T"),
-        new(Key.U, "U"),
-        new(Key.V, "V"),
-        new(Key.W, "W"),
-        new(Key.X, "X"),
-        new(Key.Y, "Y"),
-        new(Key.Z, "Z")
-    };
-
     [Fact]
     public async void LetterListener_SubscribeUnsubscribe()
     {
@@ -53,7 +23,7 @@ public sealed class LetterListenerUnsubscribeTests
     {
         var keyboardListener = new KeyboardListener();
         var letterListener = new LetterListener(keyboardListener);
-        var keys = _letKeys.Select(n => n.Key).ToArray();
+        var keys = letterListener.Keys.ToArray();
 
         await Task.Run(() =>
         {
@@ -64,7 +34,7 @@ public sealed class LetterListenerUnsubscribeTests
 
             Assert.True(keyboardListener.IsListening);
             Assert.Equal(keyboardListener.Keys.Count(), keys.Length);
-            Assert.True(keys.All(k => keyboardListener.Keys.Select(s => s.Key).Contains(k)));
+            Assert.True(keys.All(key => keyboardListener.Keys.Select(s => s.Key).Contains(key)));
             Assert.False(letterListener.IsListening);
         });
     }
